3c297553f874a05f297ac7420523bee6af785d88
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-04-25 11:06:55 +0200

    -

--- htest/fetch.js
+++ htest/fetch.js
@@ -2,7 +2,7 @@ self.addEventListener('fetch', event => {
   console.log('event ', event.request);
   if(event.request.url.endsWith('/shark')){
   event.respondWith(
-  new Response("<h1></h1>", {
+  new Response("<h1>Sharkbyte</h1>", {
     headers: {'Content-Type': 'text/html'}
   });
 }});
