fb319e983c4fdd0798cc2527b86d020234bc13ab
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-04-25 12:47:49 +0200

    t

--- htest/testswf/sw.js
+++ htest/testswf/sw.js
@@ -1,4 +1,4 @@
-var htmlc = '<body><title>Sharkbyte SW Test</title><style>body {color: blue; background-color: black;}</style><h1>Sharkbyte</h1><p><a href="../..">Main</a><hr>Super, der serviceWorker funktioniert</p></body>';
+var htmlc = '<body><title>Sharkbyte SW Test</title><style>body {color: blue; background-color: black;}</style><h1>Sharkbyte</h1><p><a href="../..">Main</a><hr>serviceWorker works</p></body>';
 self.addEventListener('fetch', event => {
   console.log('event ', event.request);
   if(event.request.url.endsWith('/shark')){
