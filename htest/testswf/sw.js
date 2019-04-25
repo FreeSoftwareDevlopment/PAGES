eb49fd95917ff500decab21cae0d8f2ce57b2adf
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-04-25 11:33:56 +0200

    -

--- htest/testswf/sw.js
+++ htest/testswf/sw.js
@@ -4,5 +4,5 @@ self.addEventListener('fetch', event => {
   event.respondWith(
   new Response("<title>Sharkbyte</title><h1>Sharkbyte</h1>", {
     headers: {'Content-Type': 'text/html'}
-  });
+  }));
 }});
